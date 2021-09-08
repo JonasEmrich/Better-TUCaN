c79d0db00febbc430252be87b163fb289fc564b5
Jonas Emrich <jonas.emrich@gmx.de>
2021-09-08 11:49:57 +0200

    imported local projectfiles

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Better TUCaN
+# Better TUCaN 
+
+## What it does
+
+This extension just includes:
+
+* the content scripts, "script.js" and "style.css" that is injected into any pages
+under "tucan.tu-darmstadt.de/" or any of its subdomains
+
+The content scripts change the website appearance of "tucan.tu-darmstadt.de/" to a dark theme and add statistics to the gradeoverview.
+
+## Credits
+The Add-On Icon is made by freepik.com from flaticon.com
+The original "TUCaN" Logo and the Logo of "Technische University Darmstadt" belongs and is property of the Technische University Darmstadt
---
+++ script.js
@@ -0,0 +1,101 @@
+function max(arr){
+  return Math.max.apply(null, arr);
+}
+
+function sum(arr){
+  return  arr.reduce((a, b) => a + b, 0);
+}
+/*
+function gradeoverview(){
+  labels = [];
+  $('table.nb').children('tbody').children('tr').eq(0).children('td').each(function(){
+    labels.push($(this).text());
+  });
+  labels.splice(0,1);
+
+  students = [];
+  $('table.nb').children('tbody').children('tr').eq(1).children('td').each(function(){
+    if(isNaN(parseInt($(this).text())))
+      students.push(0);
+    else
+       students.push(parseInt($(this).text()));
+  });
+  students.splice(0,1);
+alert(max());
+  var max = max(students);
+  var sum = sum(students);
+  var durchfallquote = Math.round(students[students.length-1]/sum*100*100)/100;
+  var bestanden = 100-durchfallquote;
+
+  if(isNaN(max) || isNaN(sum) || isNaN(durchfallquote))
+    return;
+
+  // Bestanden - Durchgefallen Progress Bar
+  $('#pageContentPopUp > div.tb').prepend('<div class="bar-wrapper"><span class="green"><b>Bestanden</b> <br>'+bestanden+'%</span><strong class="bar-label"></strong><progress class="pbar" max="100" value="'+bestanden+'"> '+bestanden+'% / '+durchfallquote+'% </progress><span><b>Durchgefallen</b> <br> '+durchfallquote+'%</span></div>');
+
+  // Notenverteilung Chart
+  $('#pageContentPopUp > div.tb').prepend('<ul id="grade-chart" class="chart-grades"></ul>');
+
+  for (var i = 0; i < students.length; i++){
+    var height = students[i]/max*100;
+    if(height == 0)
+      height += 1;
+    $('#grade-chart').append('<li><div style="height:'+  height +'%" title="'+labels[i]+'" value="'+students[i]+'"><span class="chart-value">'+students[i]+'</span></div></li>');
+  }
+
+}
+*/
+
+var src = "$('#field_user').attr('placeholder', 'TU-ID');"+
+          "$('#field_pass').attr('placeholder', 'Passwort');"+
+          "document.getElementById('field_pass').type = 'password';"+
+          "$('#inhalt p[style*=\"color: darkred\"]').css('color', 'red');";
+
+var jq = document.createElement('script');
+jq.append(src);
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+
+
+var url = window.location.href;
+
+if(url.includes('PRGNAME=GRADEOVERVIEW')){
+  //$('#pageContentPopUp > h2').eq(1).replaceWith('<h3>'+$('#pageContentPopUp > h2').eq(1).html()+'</h3>');
+
+  labels = [];
+  $('table.nb').children('tbody').children('tr').eq(0).children('td').each(function(){
+    labels.push($(this).text());
+  });
+  labels.splice(0,1);
+
+  students = [];
+  $('table.nb').children('tbody').children('tr').eq(1).children('td').each(function(){
+    if(isNaN(parseInt($(this).text())))
+      students.push(0);
+    else
+       students.push(parseInt($(this).text()));
+  });
+  students.splice(0,1);
+
+  var max = max(students);
+  var sum = sum(students);
+  var durchfallquote = Math.round(students[students.length-1]/sum*100*100)/100;
+  var bestanden = 100-durchfallquote;
+
+  if(!(isNaN(max) || isNaN(sum) || isNaN(durchfallquote))){
+
+      // Bestanden - Durchgefallen Progress Bar
+      $('#pageContentPopUp > div.tb').prepend('<div class="bar-wrapper"><span class="green"><b>Bestanden</b> <br>'+bestanden+'%</span><strong class="bar-label"></strong><progress class="pbar" max="100" value="'+bestanden+'"> '+bestanden+'% / '+durchfallquote+'% </progress><span><b>Durchgefallen</b> <br> '+durchfallquote+'%</span></div>');
+
+      // Notenverteilung Chart
+      $('#pageContentPopUp > div.tb').prepend('<ul id="grade-chart" class="chart-grades"></ul>');
+
+      for (var i = 0; i < students.length; i++){
+        var height = students[i]/max*100;
+        if(height == 0)
+          height += 1;
+        $('#grade-chart').append('<li><div style="height:'+  height +'%" title="'+labels[i]+'" value="'+students[i]+'"><span class="chart-value">'+students[i]+'</span></div></li>');
+      }
+  }
+
+}
